aa87a23d35c60bb53b5249920de4a919994f7405
Emmanuel Tar <emmanueltar14@gmail.com>
2023-09-02 17:36:37 +0100

    echo "# python_with_Telusko" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin git@github.com:emmanuel-tar/python_with_Telusko.git push -u origin main

--- functions/function_demo.py
+++ functions/function_demo.py
@@ -14,3 +14,13 @@ def add(x,y):
 result = add(5,5)
 print(result)
 greet()
+
+
+#---- Two Values return in function---
+def add_sub(x,y):
+    c=x+y
+    d=x-y
+    return c,d
+
+result1,result2 = add_sub(45,90)
+print(result1,'\n',result2)
